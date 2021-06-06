b490650cb4fb00cf256688a92ce2b97addeda3e2
Crmiguez <crmiguez@esei.uvigo.es>
2021-06-06 20:45:18 +0200

    Upload parts

---
+++ 03/js/emoji_run.js
@@ -0,0 +1,14 @@
+emojify.setConfig({
+
+    emojify_tag_type : 'div',           // Only run emojify.js on this element
+    only_crawl_id    : null,            // Use to restrict where emojify.js applies
+    img_dir          : 'images/emoji',  // Directory for emoji images
+    ignored_tags     : {                // Ignore the following tags
+        'SCRIPT'  : 1,
+        'TEXTAREA': 1,
+        'A'       : 1,
+        'PRE'     : 1,
+        'CODE'    : 1
+    }
+});
+emojify.run();
